e9bd5985b42bca11cdf02941a1fb4d89100cf5f6
7K <kmanfinkel17@gmail.com>
2023-08-26 15:16:51 -0600

    its a box

--- ThreeJSTSPractice/src/pages/game/Game.tsx
+++ ThreeJSTSPractice/src/pages/game/Game.tsx
@@ -1,16 +1,24 @@
 import './Game.scss'
 import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
+import { Link } from 'react-router-dom';
 
-
-
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh(geometry, material);
 
 export const Game = () =>{
 
 
     return(
-        <Canvas>
-
+        <>
+         <Link to='/'><button>Home</button></Link>
+        <Canvas >
+           <mesh>
+                <boxGeometry args={[1,1,1]}/>
+           </mesh>
         </Canvas>
+        </>
+       
     )
 }
